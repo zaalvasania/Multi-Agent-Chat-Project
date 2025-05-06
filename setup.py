2addfabfaa1d53e49da35900db7142ae8c70f43d
zaalvasania <zaalvasania@gmail.com>
2025-05-05 23:43:32 -0700

    Added requirements.txt file to root

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    "anthropic",
+    "together",
+    "openai",
+    "sqlalchemy",
+    "agno",
+    "tavily-python",
+    "duckduckgo-search",
+    "streamlit"
+]
+
 setup(
     name="agno_ai_multi_chat_app",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "anthropic",
-        "together",
-        "openai",
-        "sqlalchemy",
-        "agno",
-        "tavily-python",
-        "duckduckgo-search",
-        "streamlit"
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "run-multiagent-chat=agno_ai_multi_chat_app.run:main",
